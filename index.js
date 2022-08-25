0ebbc6b50d6051e7c4550bbcd106d105753463fc
uditkishor <uditkishor@buyume.io>
2022-08-25 12:31:07 +0530

    mminor fix

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const connect = require('./configs/db');
 
 var cors = require('cors');
 app.use(cors());
+
 // const userController = require("./src/Routes/user");
 // const productController = require("./src/Routes/product");
 // const brandController = require("./src/Routes/brand");
 // app.use('/users', userController);
 // app.use('/product', productController);
 // app.use('/brand', brandController);
-
-
 
 app.listen(8080, ()=>{
     connect();
